d274690f09b75d14690d4076a85ac144c997a652
Iaroslav <yar.bezruchko@gmail.com>
2024-11-01 12:18:52 +0200

    news and words scheduled to any chat participating

--- send_message.py
+++ send_message.py
@@ -6,12 +6,12 @@ import requests
 import random
 from dotenv import load_dotenv
 from telegram import Bot
+from groups_manager import load_chat_ids
 
 # Load environment variables
 load_dotenv()
 
 API_KEY = os.getenv('BOT_API_KEY')
-CHAT_ID = os.getenv('GROUP_CHAT_ID')
 GOOGLE_SHEETS_CSV_URL = os.getenv('GOOGLE_SHEETS_CSV_URL')
 
 # Initialize bot
@@ -20,28 +20,21 @@ bot = Bot(token=API_KEY)
 # Function to fetch words and translations from Google Sheets
 def fetch_words():
     response = requests.get(GOOGLE_SHEETS_CSV_URL)
-    response.raise_for_status()  # Raise an error if the request fails
-
+    response.raise_for_status()
     words = []
     csv_data = response.content.decode('utf-8').splitlines()
     reader = csv.reader(csv_data)
     next(reader)  # Skip the header
-
     for row in reader:
         if len(row) >= 2:
             words.append((row[0], row[1]))  # Append (word, translation) tuples
-
     return words
 
-# Function to send a word and its translation to the group
-def send_message():
+# Function to send a word and its translation to each group
+def send_message(chat_ids):
     words = fetch_words()
     if words:
         word, translation = random.choice(words)
         message = f"*{word}* - {translation}"
-        bot.send_message(chat_id=CHAT_ID, text=message, parse_mode='Markdown')
-    else:
-        bot.send_message(chat_id=CHAT_ID, text="No words found in the list.")
-
-if __name__ == '__main__':
-    send_message()  # This allows you to run `python send_message.py` directly for testing
+        for chat_id in chat_ids:
+            bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
